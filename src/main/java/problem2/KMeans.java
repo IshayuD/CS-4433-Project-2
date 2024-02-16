870a47f566958dbe701d42cc144cc31c115acf21
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2024-02-16 17:50:06 -0500

    calculates the new centroids for 1 iteration

--- src/main/java/problem2/KMeans.java
+++ src/main/java/problem2/KMeans.java
@@ -16,6 +16,7 @@ import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -108,33 +109,39 @@ public class KMeans {
     }
   }
 
-//  public static class IntSumReducer
-//          extends Reducer<Text,IntWritable,Text,IntWritable> {
-//    private IntWritable result = new IntWritable();
-//
-//    public void reduce(Text key, Iterable<IntWritable> values,
-//                       Context context
-//    ) throws IOException, InterruptedException {
-//      int sum = 0;
-//      for (IntWritable val : values) {
-//        sum += val.get();
-//      }
-//      result.set(sum);
-//      context.write(key, result);
-//    }
-//  }
+  public static class NewCentroidCalculatorReducer
+          extends Reducer<Text,Text,Text,Text> {
+
+    public void reduce(Text key, Iterable<Text> values,
+                       Context context
+    ) throws IOException, InterruptedException {
+      int numDataPoints = 0;
+      double xSum = 0;
+      double ySum = 0;
+      for(Text value: values) {
+        numDataPoints++;
+        String[] dataPointComponents = value.toString().split(",");
+        int x = Integer.parseInt(dataPointComponents[0]);
+        int y = Integer.parseInt(dataPointComponents[1]);
+        xSum += x;
+        ySum += y;
+      }
+      Point newCentroid = new Point((int) xSum/numDataPoints, (int) ySum/numDataPoints);
+      context.write(new Text(newCentroid.toString()), key);
+    }
+  }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "Find centroid");
     job.setJarByClass(KMeans.class);
     job.setMapperClass(ClosestCentroidMapper.class);
+    job.setReducerClass(NewCentroidCalculatorReducer.class);
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(Text.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
     job.addCacheFile(new URI("file:///C:/schoolMahir/CS4433-BigData/Project2/CS-4433-Project-2/data_set/k_centroids.csv"));
-    job.setNumReduceTasks(0);
     FileInputFormat.addInputPath(job, new Path("C:\\schoolMahir\\CS4433-BigData\\Project2\\CS-4433-Project-2\\data_set\\data_points.csv"));
     FileOutputFormat.setOutputPath(job, new Path("problem2_output"));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
